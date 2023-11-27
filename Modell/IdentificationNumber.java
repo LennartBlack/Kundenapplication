44443a7c851b635cf35b2c7b27b9ec66f1b708d8
Lennart Schwartz <lennart.schwartz@ewe-netz.de>
2023-11-27 14:31:05 +0100

    adding Class of ArrayList<Integer> for ID's

--- Modell/IdentificationNumber.java
+++ Modell/IdentificationNumber.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 abstract class IdentificationNumber {
     private static ArrayList<Integer> idList = new ArrayList<>();
 
+
     public Integer getNewID(){
     idList.add(idList.size());
      return idList.get(idList.size() - 1);
