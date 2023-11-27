fe6168f91cd9c952fefdf2e027a9dd161f4c7c84
Lennart Schwartz <lennart.schwartz@ewe-netz.de>
2023-11-27 13:51:44 +0100

    refactoring sonarlingt issues

--- Database/Database.java
+++ Database/Database.java
@@ -6,7 +6,9 @@ import java.sql.SQLException;
 
 
 public class Database {
-
+    private static final String URL = "jdbc:mysql://localhost:3306/kundenliste";
+    private static final String USER = "user_1";
+    private static final String PASSWORD = "1234";
     private Connection connection;
     public Connection getConnection() {
         return connection;
@@ -18,17 +20,13 @@ public class Database {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             // Baue die Verbindung zur Datenbank auf
-            this.connection = DriverManager.getConnection(url, user, password);
+            this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Erfolgreich mit der Datenbank verbunden!");
+            // Hier kannst du weitere Aktionen mit der Datenbank durchführen
+            // ...
 
-            // Überprüfe, ob die Verbindung erfolgreich war
-            if (connection != null) {
-                System.out.println("Erfolgreich mit der Datenbank verbunden!");
-                // Hier kannst du weitere Aktionen mit der Datenbank durchführen
-                // ...
-
-                // Schließe die Verbindung, wenn sie nicht mehr benötigt wird
-                connection.close();
-            }
+            // Schließe die Verbindung, wenn sie nicht mehr benötigt wird
+            connection.close();
         } catch (ClassNotFoundException e) {
             System.out.println("MySQL JDBC-Treiber nicht gefunden!");
             e.printStackTrace();
@@ -37,33 +35,5 @@ public class Database {
             e.printStackTrace();
         }
     }
-    // JDBC-URL, Benutzername und Passwort der MySQL-Datenbank
-    private String url = "jdbc:mysql://localhost:3306/kundenliste";
-    private String user = "user_1";
-    private String password = "1234";
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
 
 }
