852ff348343ffc580f5ba9a4c2179ab70c2aefdf
Lennart Schwartz <lennart.schwartz@ewe-netz.de>
2023-11-27 14:00:15 +0100

    adding attributes of Costumer

--- Modell/Costumer.java
+++ Modell/Costumer.java
@@ -1,4 +1,26 @@
 package Modell;
 
+import java.time.LocalDate;
 public class Costumer {
+    //Attributes
+    private final int ID;
+    private String anrede;
+    private String name;
+    private String familyName;
+    private LocalDate birthday;
+    private String street;
+    private String houseNumber;
+    private String plz;
+    private String city;
+    private String telefon;
+    private String mobil;
+    private String telefax;
+    private String email;
+    private String newsletter;
+
+    //Constructors
+
+    //Methods
+
+    //Getter & Setter
 }
